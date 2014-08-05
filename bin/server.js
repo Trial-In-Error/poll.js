95200de25df4cf61751e91c7a012ec505bae8634
Trial-In-Error <tylhandrias@gmail.com>
2014-08-05 18:04:11 +0200

    Heroku support changes.

--- bin/server.js
+++ bin/server.js
@@ -5,7 +5,7 @@ var https = require('https');
 var fs = require('fs');
 
 var port = (process.env.PORT || 3000);
-var secure_port = 443;
+/*var secure_port = 443;
 
 
 try {
@@ -15,12 +15,12 @@ try {
 	};
 } catch (err) {
 	console.log(err);
-}
+}*/
 
 http.createServer(app).listen(port);
 
-if(typeof options !== 'undefined' && typeof options.key !== 'undefined' && typeof options.key === 'object' && typeof options.cert !== 'undefined' && typeof options.cert === 'object') {
+/*if(typeof options !== 'undefined' && typeof options.key !== 'undefined' && typeof options.key === 'object' && typeof options.cert !== 'undefined' && typeof options.cert === 'object') {
 	https.createServer(options, app).listen(secure_port);
 } else {
 	console.log('HTTPS server cannot find certificate and key. Did not start.');
-}
+}*/
