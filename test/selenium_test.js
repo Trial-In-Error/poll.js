3c577229e1cde8158478f319a63e822cde9cf69c
Trial-In-Error <tylhandrias@gmail.com>
2014-09-05 10:01:02 +0200

    More selenium.

--- test/selenium_test.js
+++ test/selenium_test.js
@@ -1,9 +1,19 @@
-var webdriver = require('selenium-webdriver');
+var assert = require('assert'),
+test = require('selenium-webdriver/testing'),
+webdriver = require('selenium-webdriver');
  
-var driver = new webdriver.Builder().
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();
- 
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
-driver.findElement(webdriver.By.name('btnG')).click();
+test.describe('Google Search', function() {
+  test.it('should work', function() {
+  	this.timeout(5000);
+    var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
+driver.get('http://216.239.32.20');
+    var searchBox = driver.findElement(webdriver.By.name('q'));
+    searchBox.sendKeys('simple programmer');
+    searchBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'simple programmer');
+    });
+    driver.quit();
+  });
+});
