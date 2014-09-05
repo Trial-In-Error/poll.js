c6fbfb5f9b20475a9bbce094bb22bc6df32f78b3
Trial-In-Error <tylhandrias@gmail.com>
2014-09-05 10:15:13 +0200

    More selenium stuff. God is it SLOOOOOW!

--- test/selenium_test.js
+++ test/selenium_test.js
@@ -1,19 +1,30 @@
 var assert = require('assert'),
 test = require('selenium-webdriver/testing'),
 webdriver = require('selenium-webdriver');
- 
+var should = require('should');
+var request = require('supertest');
+
 test.describe('Google Search', function() {
-  test.it('should work', function() {
-  	this.timeout(5000);
-    var driver = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.chrome()).
-    build();
-driver.get('http://216.239.32.20');
-    var searchBox = driver.findElement(webdriver.By.name('q'));
-    searchBox.sendKeys('simple programmer');
-    searchBox.getAttribute('value').then(function(value) {
-      assert.equal(value, 'simple programmer');
-    });
-    driver.quit();
-  });
+	var driver = new webdriver.Builder().
+	withCapabilities(webdriver.Capabilities.chrome()).
+	build();
+
+	test.it('should work', function() {
+		driver.get('http://duckduckgo.com/');
+		var searchBox = driver.findElement(webdriver.By.id('search_form_input_homepage'));
+		searchBox.sendKeys('simple programmer');
+		searchBox.getAttribute('value').then(function(value) {
+			value.should.equal(value, 'simple programmer');
+		});
+	});
+
+	test.it('should not work', function() {
+		driver.get('http://duckduckgo.com/');
+		var searchBox = driver.findElement(webdriver.By.id('search_form_input_homepage'));
+		searchBox.sendKeys('simple programmers');
+		searchBox.getAttribute('value').then(function(value) {
+			value.should.not.equal('simple programmer');
+		});
+		driver.quit();
+	})
 });
