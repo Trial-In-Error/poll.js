72c51d65c41216f3ec16699f55e6b6fa53bedc66
Trial-In-Error <tylhandrias@gmail.com>
2014-09-18 12:25:37 +0200

    More readable client-side /register/ verification.

--- public/javascripts/client_register.js
+++ public/javascripts/client_register.js
@@ -2,14 +2,16 @@ $(document).on('pageinit', function() {
 	$(document).on('click', '#submit', function() {
 		var maxUsernameLength = 32;
 		var maxPasswordLength = 32;
-		if( $('#username').val().length > 0 && $('#username').val().length <= maxUsernameLength
-			&& $('#password').val().length >= 5 && $('#password').val().length <= maxPasswordLength ){
+		var username = $('#username').val();
+		var password = $('#password').val();
+		if( username.length > 0 && username.length <= maxUsernameLength
+			&& password.length >= 5 && password.length <= maxPasswordLength ){
 			// Send data to server through the ajax call
 			// action is functionality we want to call and outputJSON is our data
 			$.ajax({
 				url: '/register',
 				// WARN: This sends the password in the clear!!!
-				data: {action : 'register', username : $('#username').val(), password : $('#password').val()},
+				data: {action : 'register', username : username, password : password},
 				type: 'post',
 				async: 'true',
 				dataType: 'json'
@@ -33,18 +35,22 @@ $(document).on('pageinit', function() {
 				alert('Network error has occurred please try again!');
 			});
 		} else {
-			if($('#username').val().length <= 0 && $('#password').val().length > 0) {
+			if(username.length <= 0 && password.length > 0) {
 				alert('Please fill in your username.');
-			} else if ($('#username').val().length > 0 && $('#password').val().length <= 0) {
+			} else if (username.length > 0 && password.length <= 0) {
 				alert('Please fill in your password.');
-			} else if ($('#password').val().length < 5 && $('#username').val().length > 0) {
-				alert('Please a choose a password at least 5 characters long.');
-			} else if ($('#username').val().length > maxUsernameLength) {
+			} else if (password.length < 5 && username.length > 0) {
+				alert('Please choose a password at least 5 characters long.');
+			} else if (username.length > maxUsernameLength && password.length <= maxPasswordLength) {
 				alert('Please choose a username shorter than '+maxUsernameLength+' characters.');
-			} else if ($('#password').val().length > maxPasswordLength) {
+			} else if (username.length <= maxUsernameLength && password.length > maxPasswordLength) {
 				alert('Please choose a password shorter than '+maxPasswordLength+' characters.');
-			} else {
+			} else if (username.length > maxUsernameLength && password.length > maxPasswordLength) {
+				alert('Please choose a username shorter than '+maxUsernameLength+' characters and a password shorter than '+maxPasswordLength+' characters.');
+			} else if (username.length <= 0 && password.length <= 0) {
 				alert('Please fill in your username and password.');
+			} else {
+				alert('Something went wrong.');
 			}
 		}
 		return false; // cancel original event to prevent form submitting
