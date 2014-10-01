0f5316d39ee682882febdf030e860c45469d0c0a
Trial-In-Error <tylhandrias@gmail.com>
2014-10-01 09:21:39 +0200

    I am bad at logic.

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'development') {
 	require('nodetime').profile({
 		accountKey: 'b023cd76db5237236ddcad980a6f3bb6c02d500d',
 		appName: 'flashpoll',
