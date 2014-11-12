b167bf1ccff3af482a5b1293e87044eb26e1e9fa
Trial-In-Error <tylhandrias@gmail.com>
2014-11-12 13:01:14 +0100

    Grid throbber added.

--- public/javascripts/clientPollGrid.js
+++ public/javascripts/clientPollGrid.js
@@ -86,9 +86,35 @@ function getScrollbarWidth() {
     return widthNoScroll - widthWithScroll;
 }
 
+
+
 $(window).load(function() {
 	$('.ui-footer').hide();
 	$('.ui-header').hide();
+
+
+var opts = {
+  lines: 5, // The number of lines to draw
+  length: 29, // The length of each line
+  width: 18, // The line thickness
+  radius: 15, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 42, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1.6, // Rounds per second
+  trail: 64, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: true, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
+
+var target = document.body;
+
+var spinner = new Spinner(opts).spin(target);
 
 	var url = window.location.origin+'/pollroute/exportpolljson/'+window.location.pathname.split('grid/').pop();
 	var array = [0, 1, 2, 3, 4];
@@ -157,6 +183,7 @@ $(window).load(function() {
 		$container.css('padding', '0px');
 
 		suppressPieChartInteractions();
+		spinner.stop();
 		$container.isotope('reloadItems');
 		$container.isotope('layout');
 
