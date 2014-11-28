d27f26782d52adc9d74dfb55b73fa5b6c0d5a146
Trial-In-Error <tylhandrias@gmail.com>
2014-11-28 15:20:38 +0100

    Logout fixes.

--- routes/logout.js
+++ routes/logout.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/logout', function(req, res){
+router.get('/', function(req, res){
 	req.logout();
 	// Delete local storage of results
 	res.redirect('/');
