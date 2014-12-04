db072f6ff3142a88ab0b30413d6851674df8eb2e
Trial-In-Error <tylhandrias@gmail.com>
2014-12-04 12:46:31 +0100

    redis startup script now hooked into npm start\!

--- bin/redis_init_helper.js
+++ bin/redis_init_helper.js
@@ -2,6 +2,6 @@ var os = require('os');
 var sys = require('sys');
 var exec = require('child_process').exec;
 
-var options = 'mongod -dbpath ./data';
+var options = 'redis-server';
 function puts(error, stdout, stderr) { sys.puts(stdout); }
 exec(options, puts);
