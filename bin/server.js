64d83857a2f23e12b075bd041ede87a242dc03d9
Trial-In-Error <tylhandrias@gmail.com>
2014-12-04 13:17:37 +0100

    Disabled HTTPS for heroku reasons; trying heroku build again.

--- bin/server.js
+++ bin/server.js
@@ -32,9 +32,9 @@ try {
 	console.log('Could not find '+err.path);
 } finally {
 	http.createServer(app).listen(port);
-	if(typeof options !== 'undefined' && typeof options.key !== 'undefined' && typeof options.key === 'object' && typeof options.cert !== 'undefined' && typeof options.cert === 'object') {
-		https.createServer(options, app).listen(secure_port);
-	} else {
-		console.log('HTTPS server did not start because it could not find the certificate and key.');
-	}
+	//if(typeof options !== 'undefined' && typeof options.key !== 'undefined' && typeof options.key === 'object' && typeof options.cert !== 'undefined' && typeof options.cert === 'object') {
+	//	https.createServer(options, app).listen(secure_port);
+	//} else {
+	//	console.log('HTTPS server did not start because it could not find the certificate and key.');
+	//}
 }
